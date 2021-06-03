8e3fd59f76a6404a40c43758930df10d58a1548e
Dilain Gunasekara <adsgunasekara18@gmail.com>
2021-06-03 20:39:46 +0530

    initial  commit

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,7 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  // ENTER YOUR CONFIG HERE
+  apiKey: "AIzaSyAnC8uJG0Exh8MV34hiJhoLn1tUS9xC24Q",
+  authDomain: "diaryofsam-ab961.firebaseapp.com",
+  projectId: "diaryofsam-ab961",
+  storageBucket: "diaryofsam-ab961.appspot.com",
+  messagingSenderId: "687823357552",
+  appId: "1:687823357552:web:fa32cd5cba2864213196b5"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
